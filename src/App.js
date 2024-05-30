1f8d3ad3b870eb4d3becced6b3159f1c6e09d4c5
louaylh10 <hcinlouay@gmail.com>
2024-05-30 16:25:28 +0100

    V3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import log from './img/logo_original.png';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -26,74 +27,47 @@ function App() {
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
- 
-  const clickacc=()=>{
-    const home = document.querySelector(".Home");
-    home.scrollIntoView({ behavior: "smooth" });
-    handleCheckboxChange();
-  };
-  const clickprop=()=>{
-    const prop = document.querySelector(".Apropos");
-    prop.scrollIntoView({ behavior: "smooth" });
-    handleCheckboxChange();
-  };
-  const clickgal=()=>{
-    const gal = document.querySelector(".Gallery");
-    gal.scrollIntoView({ behavior: "smooth" });
-    handleCheckboxChange();
-  };
-  const clickcnt=()=>{
-    const cnt = document.querySelector(".Contact");
-    cnt.scrollIntoView({ behavior: "smooth" });
-    handleCheckboxChange();
-  };
-  const whatsapp=()=> {
+
+  const whatsapp = () => {
     const numeroTelephone = '+21628815988';
     const lienWhatsapp = `https://wa.me/${numeroTelephone}`;
     window.open(lienWhatsapp);
   };
- 
 
-  
   return (
-    <div className="App">
-      <header>
-        <nav className="navbar">
-          <h2>Louay industrie et maintenance</h2>
-          <div className='logo'>
-            <img src={log} id='logo' alt="Logo" />
-          </div>
-         <input type='checkbox' id='check' checked={isChecked} onChange={handleCheckboxChange} />
-          <div className='menu'>
-            <ul>
-              <span className='headitems' ref={menRef}>
-                <a href='#' onClick={clickacc}><li>Acceuil</li></a>
-                <a href='#' onClick={clickprop}><li>A Propos</li></a>
-                <a href='#' onClick={clickgal}><li>Gallery</li></a>
-                <a href='#' onClick={clickcnt}><li>Contact</li></a>
-                <label htmlFor="check" className="close-menu"><i className="fas fa-times"></i></label>
-              </span>
-              <label htmlFor="check" className="open-menu"><i className="fas fa-bars"></i></label>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <div className='space' ref={sp}>
-        
-        </div>
-        <div className='what' onClick={whatsapp}><i class="fa-brands fa-whatsapp"></i></div>
-       
-        <Home />
-        <Propos/>
-        <Gallery />
-        <Contact />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav className="navbar">
+            <h2>Louay industrie et maintenance</h2>
+            <div className='logo'>
+              <img src={log} id='logo' alt="Logo" />
+            </div>
+            <input type='checkbox' id='check' checked={isChecked} onChange={handleCheckboxChange} />
+            <div className='menu'>
+              <ul>
+                <span className='headitems' ref={menRef}>
+                  <Link to="/" onClick={handleCheckboxChange}><li>Acceuil</li></Link>
+                  <Link to="/apropos" onClick={handleCheckboxChange}><li>A Propos</li></Link>
+                  <Link to="/gallery" onClick={handleCheckboxChange}><li>Gallery</li></Link>
+                  <Link to="/contact" onClick={handleCheckboxChange}><li>Contact</li></Link>
+                  <label htmlFor="check" className="close-menu"><i className="fas fa-times"></i></label>
+                </span>
+                <label htmlFor="check" className="open-menu"><i className="fas fa-bars"></i></label>
+              </ul>
+            </div>
+          </nav>
+        </header>
+        <div className='space' ref={sp}></div>
+        <div className='what' onClick={whatsapp}><i className="fab fa-whatsapp"></i></div>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/apropos" component={Propos} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
-const scrl=()=>{
-  const props=document.querySelector(".Apropos");
-  props.scrollIntoView({ behavior: "smooth" });
- }
-  
-export {scrl};
+
 export default App;
